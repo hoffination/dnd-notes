b1e0f00715af1bca41a05eec2f8dd8408e7519c9
Ben Hofferber <hofferber.ben@gmail.com>
2018-02-10 19:28:04 -0500

    :cat2: Added wallaby configuration and Element UI

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import { Button } from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.component(Button.name, Button);
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +17,5 @@ new Vue({
   router,
   components: { App },
   template: '<App/>',
+  render: h => h(App),
 });
---
+++ wallaby.js
@@ -0,0 +1,58 @@
+// working setup for mocha tests
+const webpackConfig = require('./build/webpack.base.conf.js')
+const wallabyWebpack = require('wallaby-webpack')
+
+module.exports = function (wallaby) {
+  webpackConfig.resolve.alias = {'@': require('path').join(wallaby.projectCacheDir, 'src')}
+  webpackConfig.externals = {vue: 'Vue'}
+  webpackConfig.module.rules.find(r => r.loader === 'vue-loader').options.loaders.js = ''
+
+  const wallabyPostprocessor = wallabyWebpack(webpackConfig)
+
+  return {
+    files: [
+      {pattern: 'node_modules/vue/dist/vue.js', instrument: false},
+      {pattern: 'node_modules/babel-polyfill/dist/polyfill.js', instrument: false},
+      // {pattern: 'node_modules/babel-polyfill/browser.js', instrument: false},
+      {pattern: 'src/**/*.*', load: false},
+      {pattern: 'src/**/*.test.js', load: false, ignore: true}
+    ],
+
+    // env: {
+    //   type: 'browser'
+    // },
+
+    tests: [
+      {pattern: 'test/**/*.spec.js', load: false}, // tests from https://github.com/ChangJoo-Park/vue-wallaby-webpack-template
+      {pattern: 'src/**/*.test.js', load: false}
+    ],
+
+    postprocessor: wallabyPostprocessor,
+
+    setup: function () {
+      // eslint-disable-next-line
+      Vue.config.errorHandler = function (err) {
+        throw err
+      }
+      // eslint-disable-next-line
+      Vue.config.productionTip = false
+
+      window.__moduleBundler.loadTests()
+    },
+
+    // testFramework: 'jest',
+
+    hints: {
+      ignoreCoverage: /ignore coverage/
+    },
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel(),
+      '**/*.vue': require('wallaby-vue-compiler')(wallaby.compilers.babel({}))
+    },
+
+    debug: true,
+
+    screenshot_on_failure: true
+  }
+}

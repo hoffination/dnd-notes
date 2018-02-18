d724e85869dbcafa35b88ecbce65bb79d4d1b43a
Ben Hofferber <hofferber.ben@gmail.com>
2018-02-18 12:17:34 -0500

    :lock: Refactored note-item transform and added tests

--- src/components/NoteItem.vue
+++ src/components/NoteItem.vue
@@ -15,48 +15,14 @@
 </template>
 
 <script>
-import { take, drop } from 'ramda';
 import { mapActions } from 'vuex';
+import separateLinksInItem from '../utils/NoteTransform';
 
 export default {
   props: ['item'],
   name: 'NoteItem',
   computed: {
-    noteItems: (noteItem) => {
-      const words = noteItem.item.item.split(' ');
-      let position = 0;
-      const results = [];
-      noteItem.item.links.forEach((link) => {
-        if (position === 0 && link.startWord !== 0) {
-          results.push({
-            item: take(link.startWord, words).join(' '),
-          });
-          position = link.startWord;
-        }
-        if (link.startWord > position) {
-          results.push({
-            item: take(link.startWord - position, drop(position, words)).join(' '),
-          });
-          position = link.startWord;
-        }
-        results.push({
-          toId: link.toId,
-          item: take(link.endWord - link.startWord, drop(link.startWord, words)).join(' '),
-        });
-        position = link.endWord;
-      });
-      if (position !== 0 && position !== words.length) {
-        results.push({
-          item: take(words.length - position, drop(position, words)).join(' '),
-        });
-      }
-      if (position === 0 && words.length > 0) {
-        results.push({
-          item: words.join(' '),
-        });
-      }
-      return results;
-    },
+    noteItems: separateLinksInItem,
   },
   methods: Object.assign(
     mapActions([
---
+++ src/utils/NoteTransform.js
@@ -0,0 +1,37 @@
+import { take, drop } from 'ramda';
+
+export default function separateLinksInItem(noteItem) {
+  const words = noteItem.item.item.split(' ');
+  let position = 0;
+  const results = [];
+  noteItem.item.links.forEach((link) => {
+    if (position === 0 && link.startWord !== 0) {
+      results.push({
+        item: take(link.startWord, words).join(' '),
+      });
+      position = link.startWord;
+    }
+    if (link.startWord > position) {
+      results.push({
+        item: take(link.startWord - position, drop(position, words)).join(' '),
+      });
+      position = link.startWord;
+    }
+    results.push({
+      toId: link.toId,
+      item: take(link.endWord - link.startWord, drop(link.startWord, words)).join(' '),
+    });
+    position = link.endWord;
+  });
+  if (position !== 0 && position !== words.length) {
+    results.push({
+      item: take(words.length - position, drop(position, words)).join(' '),
+    });
+  }
+  if (position === 0 && words.length > 0) {
+    results.push({
+      item: words.join(' '),
+    });
+  }
+  return results;
+}
